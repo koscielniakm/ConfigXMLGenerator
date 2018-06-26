d4901a0976f5f84f34f14b855e168040287f1f58
Mateusz Kościelniak <koscielniakmateusz@onet.pl>
2018-06-26 23:38:37 +0200

    Completed MainStageButtonFactory.

--- src/cxg/fx/MainStageButtonFactory.java
+++ src/cxg/fx/MainStageButtonFactory.java
@@ -3,6 +3,7 @@ package cxg.fx;
 import java.util.ArrayList;
 import java.util.List;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
@@ -13,19 +14,44 @@ public class MainStageButtonFactory {
 	public static List<Button> getButtonList() {
 		List<Button> buttons = new ArrayList<>();
 		buttons.add(getJpaGeneratorButton());
+		buttons.add(getHibernateGeneratorButton());
+		buttons.add(getExitButton());
 		return buttons;
 	}
 	
 	private static Button getJpaGeneratorButton() {
 		Button jpaButton = new Button();
-		jpaButton.setText("Get persistence.xml for JPA 2.0");
-		jpaButton.setTooltip(new Tooltip("Generate persistence.xml for JPA 2.0")); //TODO tooltip text
+		jpaButton.setText("Get persistence.xml");
+		jpaButton.setTooltip(new Tooltip("Generate persistence.xml for Java Persistence API 2.0"));
 		jpaButton.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
-				//TODO action
+				new JpaGeneratorStage();
 			}
 		});
 		return jpaButton;
 	}
 
+	private static Button getHibernateGeneratorButton() {
+		Button hibernateButton = new Button();
+		hibernateButton.setText("Get hibernate.cfg.xml");
+		hibernateButton.setTooltip(new Tooltip("Generate hibernate.cfg.xml for Hibernate 5"));
+		hibernateButton.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				new HibernateGeneratorStage();
+			}
+		});
+		return hibernateButton;
+	}
+	
+	private static Button getExitButton() {
+		Button jpaButton = new Button();
+		jpaButton.setText("Exit generator");
+		jpaButton.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				Platform.exit();
+			}
+		});
+		return jpaButton;
+	}
+	
 }
