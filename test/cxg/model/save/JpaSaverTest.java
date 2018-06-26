5a0bd10043c957b8cb7967a708c84321bf9e7f44
Mateusz Kościelniak <koscielniakmateusz@onet.pl>
2018-06-26 21:11:16 +0200

    Fixed JpaSaverTest.

--- test/cxg/model/save/JpaSaverTest.java
+++ test/cxg/model/save/JpaSaverTest.java
@@ -2,56 +2,48 @@ package cxg.model.save;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import cxg.model.xmlmodel.jpa.Persistence;
 import cxg.model.xmlmodel.jpa.PersistenceUnit;
+import cxg.model.xmlmodel.jpa.Properties;
+import cxg.model.xmlmodel.jpa.Property;
 
 public class JpaSaverTest {
-
-	private Persistence persistence;
 	
 	private JpaSaver saver;
 	
 	@Before
 	public void before() {
-		persistence = new Persistence();
+		saver = new JpaSaver(getExamplePersistence());
 	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void testCreateSaverWithoutCompletedPersistenceAttributes() {
-		setCorrectPersistenceUnit();
-		saver = new JpaSaver(persistence);
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void testCreateSaverWithoutCompletedPersistenceUnit() {
-		setCorrectPersistenceAttributes();
-		saver = new JpaSaver(persistence);
-	}
-	
+
 	@Test
-	public void testSaveXmlFile() throws IOException {
-		setCorrectPersistenceUnit();
-		setCorrectPersistenceAttributes();
-		saver = new JpaSaver(persistence);
+	public void testSavePersistenceXmlFile() throws IOException {
 		saver.save("");
 		File xmlFile = new File("persistence.xml");
 		if (!xmlFile.exists()) throw new IOException();
 		xmlFile.delete();
 	}
 	
-	private void setCorrectPersistenceAttributes() {
-		persistence.setXmlns("test");
-		persistence.setXmlnsXsi("test");
-		persistence.setXsiSchemaLocation("test");
-		persistence.setVersion("test");
+	private Persistence getExamplePersistence() {
+		return new Persistence(getExamplePersistenceUnit());
 	}
 	
-	private void setCorrectPersistenceUnit() {
-		persistence.setPersistenceUnit(new PersistenceUnit());
+	private PersistenceUnit getExamplePersistenceUnit() {
+		return new PersistenceUnit(getExampleProperties(), "persistenceUnitName");
+	}
+	
+	private Properties getExampleProperties() {
+		List<Property> propertyList = new ArrayList<Property>();
+		propertyList.add(new Property("name1", "val1"));
+		propertyList.add(new Property("name2", "val2"));
+		propertyList.add(new Property("name3", "val3"));
+		return new Properties(propertyList);
 	}
 	
 }
